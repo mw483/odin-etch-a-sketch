45c93499932b0217a6a5c3a63bb52535c842a5ea
mw483 <mikaelwijaya888@gmail.com>
2025-03-15 23:38:10 +0900

    adding 4 boxes to a row

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,14 @@
+var box = document.createElement('div');   
+box.className = 'box'; 
+const fragment = new DocumentFragment();
 
-for (var i = 0; i < 16; i++)  {
-    var box = document.createElement('div');   
-    box.className = 'box'; 
-    document.getElementById('grid-container').appendChild(box);
+for (var i = 0; i < 4; i++) {
+    fragment.appendChild(box.cloneNode(true));
 }
 
+for (var j = 0; j < 4; j++)  {
+    var boxRow = document.createElement('div');   
+    boxRow.className = 'box-row'; 
+    document.getElementById('grid-container').appendChild(boxRow);
+    boxRow.appendChild(fragment);
+}
