6f40b764c2460eac40070348a27dad6a1b006220
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-03-16 21:36:12 +0900

    generate button v1 added

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,19 @@
-
 /*Generates the grid, by row and then insert the squares in each row*/
-for (var i = 0; i < 4; i++)  {
-    var gridRow = document.createElement('div');   
-    gridRow.className = 'grid-row'; 
-    document.getElementById('grid-container').appendChild(gridRow);
-    for (var j = 0; j < 4; j++){
-        var gridSquare = document.createElement('div');   
-        gridSquare.className = 'grid-square';
-        gridRow.appendChild(gridSquare).cloneNode(true); 
+function generateGrid() {
+    for (var i = 0; i < 4; i++)  {
+        var gridRow = document.createElement('div');   
+        gridRow.className = 'grid-row'; 
+        document.getElementById('grid-container').appendChild(gridRow);
+        for (var j = 0; j < 4; j++){
+            var gridSquare = document.createElement('div');   
+            gridSquare.className = 'grid-square';
+            gridRow.appendChild(gridSquare).cloneNode(true); 
+        }
     }
-}
+};
+
+var generateButton = document.getElementById("generate-button");
+
+generateButton.onclick = generateGrid;
+
+
