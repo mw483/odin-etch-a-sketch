30b06366b9efbafc0e50f8439cccdefbff30f4d0
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-03-16 10:46:42 +0900

    Generate 16x16 grid successfully

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,15 +1,12 @@
-var box = document.createElement('div');   
-box.className = 'box'; 
-const fragment = new DocumentFragment();
 
-for (var i = 0; i < 4; i++) {
-    fragment.appendChild(box.cloneNode(true));
+/*Generates the grid, by row and then insert the squares in each row*/
+for (var i = 0; i < 4; i++)  {
+    var gridRow = document.createElement('div');   
+    gridRow.className = 'grid-row'; 
+    document.getElementById('grid-container').appendChild(gridRow);
+    for (var j = 0; j < 4; j++){
+        var gridSquare = document.createElement('div');   
+        gridSquare.className = 'grid-square';
+        gridRow.appendChild(gridSquare).cloneNode(true); 
+    }
 }
-
-for (var j = 0; j < 4; j++)  {
-    var boxRow = document.createElement('div');   
-    boxRow.className = 'box-row'; 
-    document.getElementById('grid-container').appendChild(boxRow);
-    boxRow.appendChild(fragment);
-}
-
