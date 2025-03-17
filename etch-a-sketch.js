ef54647ec908cbc8c4b2b4fa7aa787b1a61a5f60
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-03-17 10:50:58 +0900

    set grid size and generate grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,23 @@
 /*Generates the grid, by row and then insert the squares in each row*/
+const gridSizeDefault = 16;
+let gridSize = gridSizeDefault;
+
+function setGridSize() {
+    let gridSize = prompt("Input grid size (max: 100");
+    while (gridSize > 100 || gridSize < 1) {
+        alert("invalid grid size")
+        gridSize = prompt("Input grid size (max: 100");
+    }
+    return gridSize
+}
+
 function generateGrid() {
-    for (var i = 0; i < 4; i++)  {
+    setGridSize();
+    for (var i = 0; i < gridSize; i++)  {
         var gridRow = document.createElement('div');   
         gridRow.className = 'grid-row'; 
         document.getElementById('grid-container').appendChild(gridRow);
-        for (var j = 0; j < 4; j++){
+        for (var j = 0; j < gridSize; j++){
             var gridSquare = document.createElement('div');   
             gridSquare.className = 'grid-square';
             gridRow.appendChild(gridSquare).cloneNode(true); 
