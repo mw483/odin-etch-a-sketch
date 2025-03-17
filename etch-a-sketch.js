e67ce575c65768e85fa543833ecbe7e787c0fa95
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-03-17 11:06:16 +0900

    fix setGridSize

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,22 +1,23 @@
-/*Generates the grid, by row and then insert the squares in each row*/
-const gridSizeDefault = 16;
-let gridSize = gridSizeDefault;
 
+/*Set grid size */
 function setGridSize() {
-    let gridSize = prompt("Input grid size (max: 100");
+    let gridSize = prompt("Input grid size (max: 100)");
     while (gridSize > 100 || gridSize < 1) {
-        alert("invalid grid size")
-        gridSize = prompt("Input grid size (max: 100");
+        alert("Invalid grid size")
+        gridSize = prompt("Input grid size (max: 100)");
     }
     return gridSize
 }
 
+/*Generates the grid, by row and then insert the squares in each row*/
 function generateGrid() {
-    setGridSize();
+    const gridContainer = document.getElementById("grid-container");
+    gridContainer.innerHTML = '';
+    gridSize = setGridSize();
     for (var i = 0; i < gridSize; i++)  {
         var gridRow = document.createElement('div');   
         gridRow.className = 'grid-row'; 
-        document.getElementById('grid-container').appendChild(gridRow);
+        gridContainer.appendChild(gridRow);
         for (var j = 0; j < gridSize; j++){
             var gridSquare = document.createElement('div');   
             gridSquare.className = 'grid-square';
