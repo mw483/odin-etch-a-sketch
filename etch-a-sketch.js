2f31c7d3a900c8c9f7e30a2ab5e9c64532458662
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-03-23 16:10:13 +0900

    color stuff & grid size input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,29 @@
+/*Default values*/
+const defaultGridSize = 16
+const defaultColor = 'rgb(0,0,0)'
+
+function random (number) {
+    return Math.floor(Math.random()*(number+1))
+}
+
+var rainbowColor = `rgb(${random(255)},${random(255)},${random(255)})`
+
 /*Set grid size */
 function setGridSize() {
-    let gridSize = prompt("Input grid size (max: 100)");
+    let gridSize = document.getElementById('size-input').value;
     while (gridSize > 100 || gridSize < 1) {
-        alert("Invalid grid size")
-        gridSize = prompt("Input grid size (max: 100)");
+        alert("Invalid grid size, reverting to default 16x16")
+        gridSize = defaultGridSize
     }
     return gridSize
+}
+
+function setColor () {
+    var newColor = defaultColor;
+}
+
+function changeColor() {
+
 }
 
 /*Generates the grid, by row and then insert the squares in each row*/
@@ -26,6 +44,9 @@ function generateGrid() {
         gridSquare.style.width = `${gridWidth}px`
         gridContainer.appendChild(gridSquare).cloneNode(true); 
     }
+    gridSquare.addEventListener("mouseover", () => {
+            gridSquare.classList.add("colored-class");
+        })
 };
 
 function getGridWidth () {
@@ -40,40 +61,6 @@ function getGridHeight () {
     var sizeInfo = grid.getBoundingClientRect();
     var height = sizeInfo.height;
     return height
-}
-
-const gridContainer = document.getElementById("grid-container");
-
-/* gridContainer.addEventListener("mousedown", () => {
-    for (let i = 0; i < sketchGrid.length; i++) {
-        sketchGrid[i].addEventListener("mouseover", function() {
-                sketchGrid[i].classList.add("colored-class");
-            });
-    };
-});
-
-/* window.addEventListener("mouseup", () => {
-    for (let i = 0; i < sketchGrid.length; i++) {
-        sketchGrid[i].removeEventListener("mouseover", function() {
-                sketchGrid[i].classList.add("colored-class");
-        });
-    };
-}); */
-
-let isMouseDown = false;
-document.addEventListener('mousedown', () => isMouseDown = true);
-document.addEventListener('mouseup', () => isMouseDown = false);
-
-function sketch() {
-    /* if (!isMouseDown) return;
-    else { */
-    const sketchGrid = document.querySelectorAll('.grid-square');
-    for (let i = 0; i < sketchGrid.length; i++) {
-        sketchGrid[i].addEventListener("mouseover", () => {
-            sketchGrid[i].classList.add("colored-class");
-        })
-    };
-    /* } */
 }
 
 /* function erase() {
@@ -96,8 +83,8 @@ function clearGrid(){
 var generateButton = document.getElementById("generate-button");
 generateButton.onclick = generateGrid;
 
-var sketchButton = document.getElementById("sketch-button")
-sketchButton.onclick = sketch;
+/* var sketchButton = document.getElementById("sketch-button")
+sketchButton.onclick = sketch; */
 
 /* var eraserButton = document.getElementById("eraser-button")
 sketchButton.onclick = erase;
