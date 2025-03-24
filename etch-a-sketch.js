d5cb2dce80944d007d49fc29d2cb0b172a495723
mw483 <wijaya.m.ab@m.titech.ac.jp>
2025-03-24 13:14:58 +0900

    many color modes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,10 @@
 /*Default values*/
 const defaultGridSize = 16;
 const defaultColor = 'rgb(0,0,0)';
-var currentColor = defaultColor;
 const defaultMode = 'pencil';
+var currentColor = defaultColor;
+var currentMode = 'pencil';
+var currentGridSize = defaultGridSize;
 
 let isMouseDown = false;
 document.getElementById('grid-container').onmousedown = () => (isMouseDown = true);
@@ -13,7 +15,24 @@ function random (number) {
     return Math.floor(Math.random()*(number+1));
 }
 
-var rainbowColor = `rgb(${random(255)},${random(255)},${random(255)})`;
+function randomColor () {
+    var rainbowColor = `rgb(${random(255)},${random(255)},${random(255)})`;
+    return rainbowColor
+}
+
+function nineColor () {
+    const nineColor = ["#49c0ec",
+"#a3cc54",
+"#e67ea3",
+"#8c79b4",
+"#ffc56e",
+"#6dcdb8",
+"#fefefe",
+"#e62722",
+"#2253a3"]
+    return nineColor[Math.floor(Math.random() * nineColor.length)]
+}
+
 
 /*Set grid size */
 function setGridSize() {
@@ -21,16 +40,41 @@ function setGridSize() {
     while (gridSize > 100 || gridSize < 1) {
         alert("Invalid grid size, reverting to default 16x16")
         gridSize = defaultGridSize
-    }
+    };
+    currentGridSize = gridSize;
     return gridSize
 }
 
-var currentGridSize = setGridSize();
+/* NOT WORKING YET. EASTER EGG IF current grid size = 9
+/* var isTwiceButton 
+ if (currentGridSize === '9') {
+    const buttonContainer = document.getElementById('button-container')
+    var twiceButton = document.createElement('button');
+    twiceButton.type = 'button';
+    twiceButton.className = 'button-1';
+    twiceButton.id = 'twice-button';
+    twiceButton.textContent = 'TWICE!';
+    buttonContainer.appendChild(twiceButton);
+    isTwiceButton = true;
+}
+/* else if (currentGridSize !== '9' && isTwiceButton === true) {
+    buttonContainer.removeChild(twiceButton);
+    isTwiceButton = false;
+} */
 
 function changeColor(e) {
     if (e.type === 'mouseover' && !isMouseDown) return
-    else {
-        e.target.style.backgroundColor = currentColor;
+    else if (currentMode === 'pencil')  {
+        e.target.style.backgroundColor = defaultColor;
+    }
+    else if (currentMode === 'eraser')  {
+        e.target.style.backgroundColor = '#FFFFFF';
+    }
+    else if (currentMode === 'rainbow') {
+        e.target.style.backgroundColor = randomColor();
+    }
+    else if (currentMode === 'twice') {
+        e.target.style.backgroundColor = nineColor();
     }
 }
 
@@ -56,29 +100,6 @@ function generateGrid(gridSize) {
     }
 };
 
-function getGridWidth () {
-    var grid = document.getElementsByClassName("grid-square")[0];
-    var sizeInfo = grid.getBoundingClientRect();
-    var width = sizeInfo.width;
-    return width
-}
-
-function getGridHeight () {
-    var grid = document.getElementsByClassName("grid-square")[0];
-    var sizeInfo = grid.getBoundingClientRect();
-    var height = sizeInfo.height;
-    return height
-}
-
-/* function erase() {
-    const sketchGrid = document.querySelectorAll('.grid-square')
-    for (let i = 0; i < sketchGrid.length; i++) {
-        sketchGrid[i].addEventListener("mouseover", function() {
-                sketchGrid[i].classList.remove("colored-class");
-            });
-    };
-} */
-
 function clearGrid(){
     const gridContainer = document.getElementById("grid-container");
     gridContainer.innerHTML = '';
@@ -93,11 +114,21 @@ var clearButton = document.getElementById("clear-button")
 clearButton.onclick = clearGrid;
 
 var pencilButton = document.getElementById("pencil-button")
-pencilButton.onclick = function () {
-    currentColor = defaultColor
+pencilButton.onclick = () => {
+    currentMode = 'pencil'
+};
+
+var rainbowButton = document.getElementById("rainbow-button")
+rainbowButton.onclick = () => {
+    currentMode = 'rainbow'
+};
+
+var twiceButton = document.getElementById("twice-button")
+twiceButton.onclick = () => {
+    currentMode = 'twice'
 };
 
 var eraserButton = document.getElementById("eraser-button")
-eraserButton.onclick = function () {
-    currentColor = '#FFFFFF'
+eraserButton.onclick = () => {
+    currentMode = 'eraser'
 };
